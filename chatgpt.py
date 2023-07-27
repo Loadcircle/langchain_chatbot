66d8f7b15ceb8825d457b13f88241d0483b73f3a
Jesus Milano <jesusmilano96@gmail.com>
2023-07-27 16:00:10 -0500

    upload memory history

--- chatgpt.py
+++ chatgpt.py
@@ -13,18 +13,37 @@ from langchain.memory import ConversationBufferMemory
 
 os.environ["OPENAI_API_KEY"] = constants.APIKEY
 
+try:
+    from memory_history import memoryHistory
+except ImportError:
+    memoryHistory = []
+
 if len(sys.argv) > 1:
     question = sys.argv[1]
 else:
     print("Se debe indicar una pregunta.")
     sys.exit(1) 
 
+def guardar_memory_history():
+    # Guardar la variable memoryHistory en el archivo memory_history.py
+    with open('memory_history.py', 'w', encoding='utf-8') as file:
+        file.write(f"memoryHistory = {memoryHistory}")
+
+if question == "refresh":
+    print("Memoria eliminada")
+    memoryHistory = []
+    guardar_memory_history()
+    sys.exit(1) 
+
+
+#production code -------------------------------------------------------
+        
 loader = Docx2txtLoader('data/Valia-qa.docx')
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=1)
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 data = loader.load()
 
 # Split
-text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 0)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 0)
 all_splits = text_splitter.split_documents(data)
 
 # Store 
@@ -38,7 +57,7 @@ QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"], templa
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
 #set previous memory history
-for input, output in prompt.memoryHistory:
+for input, output in memoryHistory:
     memory.save_context({"input": input}, {"output": output})
 
 chain_kwargs = {
@@ -49,6 +68,9 @@ chat = ConversationalRetrievalChain.from_llm(llm, retriever=vectorstore.as_retri
 result = chat({"question": question})
 
 answer = result['answer']
+
+memoryHistory.append((question, answer))
+guardar_memory_history()
 
 print('----------------------------------------------------')
 print(answer)
--- prompt.py
+++ prompt.py
@@ -6,8 +6,3 @@ template = """Eres un chatbot asistente amistoso y educado.
     {context}
     Pregunta: {question}
     Respuesta:"""
-
-memoryHistory = [
-    ('Que es Valia?', 'Valia es una empresa de Software y tecnología especialmente diseñada para Agentes Inmobiliarios.'),
-    ('Que se puede hacer con valia?', 'En Valia puedes crear tu perfil como agente inmobiliario, publicar anuncios de propiedades ilimitados de forma gratuita'),
-]
